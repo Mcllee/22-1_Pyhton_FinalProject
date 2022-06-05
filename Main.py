98e5281b9076883f9d20c6ea1265ba2cbace4528
Myungchul.Lee <mgcllee@gmail.com>
2022-06-05 22:16:22 +0900

    Modifications
    Modify file storage method,
    Add a list of commands

--- Main.py
+++ Main.py
@@ -1,31 +1,28 @@
-# 2022-1 Python Final Project
-
 import telegram
 from telegram.ext import Updater
 from telegram.ext import MessageHandler, Filters
-from selenium import webdriver
 import time
-import os
-
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 
 import pytz
 import datetime
 import schedule
-from selenium.webdriver.common.by import By
 
 import WebLogin as WL
 
 token = '5374345704:AAGQzrf8WsaUbYhV-_R0pvqlH-F0arA3kEw'
 id = 5312362183
 bot = telegram.Bot(token)
+WL.first_setting()
 
 
 def job():
     now = datetime.datetime.now(pytz.timezone('Asia/Seoul'))
     days = ['월', '화', '수', '목', '금', '토', '일']
     weekday = days[datetime.date(now.year, now.month, now.day).weekday()]
+
+    # 매번 갱신해 줄 정보들
+    if weekday == '월' and now.hour < 11:
+        WL.restaurant()  # 식당 정보를 월요일 갱신
 
     if weekday != '토' and weekday != '일':
         subs = WL.set_sub()
@@ -56,14 +53,19 @@ def handler(update, context):
         schedule_loop = False
         updater.stop()
         exit()
+    elif user_text == '도움말' or user_text == '명령어' or user_text == '설명':
+        order_list = '1. 내일 수업:    내일 시간표를 알려드려요!\n' \
+                     '2. 일정:        오늘의 수업 시간표를 알려드려요!\n' \
+                     '3. 수업:        e-Class에서 해야할 일들을 알려드려요!\n' \
+                     '4. 식당:        팁 지하/E동 1층 식당의 메뉴를 알려드려요!'
+        bot.send_message(chat_id=id, text=order_list)
     elif user_text == '내일수업' or user_text == '내일 수업' or user_text == '내일':
         tomororow_sub = WL.tomorrow_sub()
         tomororow_sub = tomororow_sub.split(' ')
         sum_text = '[내일 수업 알림]\n\n'
         for name, week, hour in zip(tomororow_sub[::3], tomororow_sub[1::3], tomororow_sub[2::3]):
             sum_text += f'과목: {name}\n시간: {hour} ({week})\n\n'
-        bot.send_message(chat_id=id,
-                         text=sum_text)
+        bot.send_message(chat_id=id, text=sum_text)
     elif user_text == '일정':
         now = datetime.datetime.now(pytz.timezone('Asia/Seoul'))
         todo_list = f'[{now.month}월 {now.day}일 해야할 일]\n' + WL.get_todo_list()
@@ -71,30 +73,10 @@ def handler(update, context):
     elif user_text == "수업":
         bot.send_message(chat_id=id, text=WL.set_sub())
     elif user_text == "식당" or user_text == "밥" or user_text == "학식" or user_text == "ㅅㄷ" or user_text == "ㅂ" or user_text == "ㅎㅅ":
+        WL.restaurant()
         bot.send_message(chat_id=id, text="이번주 식단입니다.")
-        chrome_options01 = webdriver.ChromeOptions()
-        chrome_options02 = webdriver.ChromeOptions()
-        chrome_options01.add_argument("--headless")
-        chrome_options01.add_argument("--no-sandbox")
-        chrome_options01.add_argument("--disable-dev-shm-usage")
-        chrome_options02.add_argument("--headless")
-        chrome_options02.add_argument("--no-sandbox")
-        chrome_options02.add_argument("--disable-dev-shm-usage")
-        # chromedriver 위치 주의!
-        driver01 = webdriver.Chrome(service=Service("chromedriver"), options=chrome_options01)
-        driver02 = webdriver.Chrome(service=Service("chromedriver"), options=chrome_options02)
-        driver01.get('https://ibook.kpu.ac.kr/Viewer/menu01')
-        time.sleep(1)  # 스크린샹을 위해 잠시 멈춤(쉡 로딩)
-        driver01.get_screenshot_as_file('screenshot01.png')
-        driver02.get('https://ibook.kpu.ac.kr/Viewer/menu02')
-        time.sleep(1)  # 스크린샹을 위해 잠시 멈춤(쉡 로딩)
-        driver02.get_screenshot_as_file('screenshot02.png')
-        driver01.close()
-        driver02.close()
         bot.send_photo(chat_id=id, photo=open('screenshot01.png', 'rb'))
         bot.send_photo(chat_id=id, photo=open('screenshot02.png', 'rb'))
-        os.remove('./screenshot01.png')
-        os.remove('./screenshot02.png')
     else:
         try:
             bot.send_message(chat_id=id, text=eval(user_text))
--- WebLogin.py
+++ WebLogin.py
@@ -3,6 +3,8 @@ from selenium.webdriver.common.by import By
 import time
 import datetime
 import pytz
+from selenium.webdriver.chrome.service import Service
+import os
 
 
 class class_inf:
@@ -13,14 +15,22 @@ class class_inf:
         self.hour = hour
 
 
+# 웹 스크랩 정보를 담는 str
+elms = ''
 # 과목 정보를 담는 리스트
 subjects = []
 
 
-def set_sub():
-    subjects.clear()
+def first_setting():
+    global elms
 
     driver = webdriver.Chrome()
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
     driver.get('http://eclass.tukorea.ac.kr/ilos/main/member/login_form.acl')
     driver.find_element(By.XPATH, '//*[@id="usr_id"]').click()
     driver.find_element(By.XPATH, '//*[@id="usr_id"]').send_keys('2018184020')
@@ -32,9 +42,11 @@ def set_sub():
     time.sleep(1)
     elms = driver.find_element(By.XPATH, '//*[@id="lecture_list"]/div[1]/div[1]').text
     driver.close()
-
     elms = elms.split('\n')
 
+
+def set_sub():
+    subjects.clear()
     for name, professor, hour in zip(elms[1::3], elms[2::3], elms[3::3]):
         subjects.append(class_inf(name, professor, hour))
 
@@ -58,22 +70,6 @@ def set_sub():
 
 def tomorrow_sub():
     subjects.clear()
-
-    driver = webdriver.Chrome()
-    driver.get('http://eclass.tukorea.ac.kr/ilos/main/member/login_form.acl')
-    driver.find_element(By.XPATH, '//*[@id="usr_id"]').click()
-    driver.find_element(By.XPATH, '//*[@id="usr_id"]').send_keys('2018184020')
-    driver.find_element(By.XPATH, '//*[@id="usr_pwd"]').click()
-    driver.find_element(By.XPATH, '//*[@id="usr_pwd"]').send_keys('1077111')
-    driver.find_element(By.XPATH, '//*[@id="login_btn"]').click()
-    driver.find_element(By.XPATH, '//*[@id="quick-menu-index"]/a[1]/div/img').click()
-
-    time.sleep(1)
-    elms = driver.find_element(By.XPATH, '//*[@id="lecture_list"]/div[1]/div[1]').text
-    driver.close()
-
-    elms = elms.split('\n')
-
     for name, professor, hour in zip(elms[1::3], elms[2::3], elms[3::3]):
         subjects.append(class_inf(name, professor, hour))
 
@@ -110,3 +106,42 @@ def get_todo_list():
     driver.close()
 
     return elms.replace('[', '\n[')
+
+
+def restaurant():
+    now = datetime.datetime.now(pytz.timezone('Asia/Seoul'))
+    days = ['월', '화', '수', '목', '금', '토', '일']
+    today = days[datetime.date(now.year, now.month, now.day).weekday()]
+
+    if today == '월' and now.hour == 10:
+        os.remove('./screenshot01.png')
+        os.remove('./screenshot02.png')
+
+    try:
+        open('screenshot01.png').close()
+        open('screenshot02.png').close()
+    except:
+        chrome_options01 = webdriver.ChromeOptions()
+        chrome_options02 = webdriver.ChromeOptions()
+        chrome_options01.add_argument("--headless")
+        chrome_options01.add_argument("--no-sandbox")
+        chrome_options01.add_argument("--disable-dev-shm-usage")
+        chrome_options02.add_argument("--headless")
+        chrome_options02.add_argument("--no-sandbox")
+        chrome_options02.add_argument("--disable-dev-shm-usage")
+
+        # chromedriver 위치 주의!
+        driver01 = webdriver.Chrome(service=Service("chromedriver"), options=chrome_options01)
+        driver02 = webdriver.Chrome(service=Service("chromedriver"), options=chrome_options02)
+
+        driver01.get('https://ibook.kpu.ac.kr/Viewer/menu01')
+        time.sleep(1)  # 스크린샹을 위해 잠시 멈춤(웹 로딩)
+        driver01.get_screenshot_as_file('screenshot01.png')
+
+        driver02.get('https://ibook.kpu.ac.kr/Viewer/menu02')
+        time.sleep(1)  # 스크린샹을 위해 잠시 멈춤(웹 로딩)
+        driver02.get_screenshot_as_file('screenshot02.png')
+
+        driver01.close()
+        driver02.close()
+
